abbff315c730d9171858a1722ac33c744d3ddb40
GhulamMustafa3 <gmustafafaisal658@gmail.com>
2025-09-02 21:59:36 +0500

    Add Dashboard And Implement Manager Functionality

--- lib/api/users.tsx
+++ lib/api/users.tsx
@@ -104,3 +104,16 @@ export const deleteUser = async (id: string) => {
     console.log(`DELETE /api/users/${id} completed`);
   }
 };
+export const fetchSessions = async () => {
+  try {
+    const res = await fetch("http://localhost:8008/api/sessions", {
+      credentials: "include",
+    });
+    if (!res.ok) throw new Error("Network response was not ok");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching sessions:", error);
+    return [];
+  }
+};
